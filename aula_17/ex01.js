39ec2a01ccbabbe6869b9f3966d3a1389142ef9b
Gigizinhalinda <163425188+Gigizinhalinda@users.noreply.github.com>
2024-10-17 11:28:15 -0300

    aulas

---
+++ aula_17/ex01.js
@@ -0,0 +1,23 @@
+function somaSerie(cont){
+    let soma = 0 
+
+
+    return soma 
+
+}
+
+console.log(somaSErie(3)) 
+console.log(somaSerie(4))
+
+
+cont = 5 
+
+let soma = 0
+                  y
+soma = soma + (1 * 1) 
+soma = soma + (2 * 3)
+soma = soma + (3 * 5)
+soma = soma + (4 * 7)
+soma = soma + (5 * 9)
+
+console.log(soma)
